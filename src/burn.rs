7e395ab28ca80b7c51c02a74970c1a49873b1152
austin <galaky2005@gmail.com>
2024-11-06 17:29:42 -0500

    initial commit; base implementation; burn implementation;

---
+++ src/burn.rs
@@ -0,0 +1,173 @@
+use std::error::Error;
+use burn::tensor::{quantization::{AffineQuantization, SymmetricQuantization}, DType, TensorData};
+use crate::ByteCon;
+
+impl ByteCon for DType {
+    fn append_to_bytes(&self, bytes: &mut Vec<u8>) -> Result<(), Box<dyn Error>> {
+
+        // enum variant byte
+        match self {
+            burn::tensor::DType::F64 => {
+                bytes.push(0);
+            },
+            burn::tensor::DType::F32 => {
+                bytes.push(1);
+            },
+            burn::tensor::DType::F16 => {
+                bytes.push(2);
+            },
+            burn::tensor::DType::BF16 => {
+                bytes.push(3);
+            },
+            burn::tensor::DType::I64 => {
+                bytes.push(4);
+            },
+            burn::tensor::DType::I32 => {
+                bytes.push(5);
+            },
+            burn::tensor::DType::I16 => {
+                bytes.push(6);
+            },
+            burn::tensor::DType::I8 => {
+                bytes.push(7);
+            },
+            burn::tensor::DType::U64 => {
+                bytes.push(8);
+            },
+            burn::tensor::DType::U32 => {
+                bytes.push(9);
+            },
+            burn::tensor::DType::U8 => {
+                bytes.push(10);
+            },
+            burn::tensor::DType::Bool => {
+                bytes.push(11);
+            },
+            burn::tensor::DType::QFloat(strategy) => {
+                bytes.push(12);
+                match strategy {
+                    burn::tensor::quantization::QuantizationStrategy::PerTensorAffineInt8(affine_quantization) => {
+                        bytes.push(0);
+
+                        // f32
+                        affine_quantization.scale.append_to_bytes(bytes)?;
+
+                        // i8
+                        affine_quantization.offset.append_to_bytes(bytes)?;
+                    },
+                    burn::tensor::quantization::QuantizationStrategy::PerTensorSymmetricInt8(symmetric_quantization) => {
+                        bytes.push(1);
+
+                        // f32
+                        symmetric_quantization.scale.append_to_bytes(bytes)?;
+                    },
+                }
+            },
+        }
+        Ok(())
+    }
+    fn extract_from_bytes(bytes: &Vec<u8>, index: &mut usize) -> Result<Self, Box<dyn Error>> where Self: Sized {
+        
+        let dtype_index = u8::extract_from_bytes(bytes, index)?;
+
+        let dtype = match dtype_index {
+            0 => {
+                burn::tensor::DType::F64
+            },
+            1 => {
+                burn::tensor::DType::F32
+            },
+            2 => {
+                burn::tensor::DType::F16
+            },
+            3 => {
+                burn::tensor::DType::BF16
+            },
+            4 => {
+                burn::tensor::DType::I64
+            },
+            5 => {
+                burn::tensor::DType::I32
+            },
+            6 => {
+                burn::tensor::DType::I16
+            },
+            7 => {
+                burn::tensor::DType::I8
+            },
+            8 => {
+                burn::tensor::DType::U64
+            },
+            9 => {
+                burn::tensor::DType::U32
+            },
+            10 => {
+                burn::tensor::DType::U8
+            },
+            11 => {
+                burn::tensor::DType::Bool
+            },
+            12 => {
+
+                let strategy_index = u8::extract_from_bytes(bytes, index)?;
+
+                match strategy_index {
+                    0 => {
+                        
+                        let scale = f32::extract_from_bytes(bytes, index)?;
+
+                        let offset = i8::extract_from_bytes(bytes, index)?;
+
+                        burn::tensor::DType::QFloat(burn::tensor::quantization::QuantizationStrategy::PerTensorAffineInt8(AffineQuantization::init(scale, offset)))
+                    },
+                    1 => {
+                        let scale = f32::extract_from_bytes(bytes, index)?;
+
+                        burn::tensor::DType::QFloat(burn::tensor::quantization::QuantizationStrategy::PerTensorSymmetricInt8(SymmetricQuantization::init(scale)))
+                    },
+                    _ => {
+                        panic!("Unexpected byte representing strategy.");
+                    },
+                }
+            },
+            _ => {
+                panic!("Unexpected byte representing tensor DType");
+            },
+        };
+        
+        Ok(dtype)
+    }
+}
+
+impl ByteCon for TensorData {
+    fn append_to_bytes(&self, bytes: &mut Vec<u8>) -> Result<(), Box<dyn Error>> {
+
+        // slice
+        self.bytes.append_to_bytes(bytes)?;
+
+        // slice
+        self.shape.append_to_bytes(bytes)?;
+
+        // dtype
+        self.dtype.append_to_bytes(bytes)?;
+
+        Ok(())
+    }
+    fn extract_from_bytes(bytes: &Vec<u8>, index: &mut usize) -> Result<Self, Box<dyn Error>> where Self: Sized {
+        
+        // slice
+        let tensor_data_bytes = Vec::<u8>::extract_from_bytes(bytes, index)?;
+
+        // slice
+        let shape = Vec::<usize>::extract_from_bytes(bytes, index)?;
+
+        // dtype
+        let dtype = DType::extract_from_bytes(bytes, index)?;
+
+        Ok(TensorData {
+            bytes: tensor_data_bytes,
+            shape,
+            dtype,
+        })
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,303 @@
+use std::error::Error;
+
+// TODO add a version byte at the front of each append_to_bytes call
+//      this can be used to match on within the extract so that changes in format across versions of this crate are unaffected
+
+#[cfg(feature = "burn")]
+pub mod burn;
+
+trait ByteCon {
+    fn append_to_bytes(&self, bytes: &mut Vec<u8>) -> Result<(), Box<dyn Error>>;
+    fn extract_from_bytes(bytes: &Vec<u8>, index: &mut usize) -> Result<Self, Box<dyn Error>> where Self: Sized;
+}
+
+#[derive(thiserror::Error, Debug)]
+enum ByteConError {
+    #[error("Index {index} out of range of bytes array with length {length}.")]
+    IndexOutOfRange {
+        index: usize,
+        length: usize,
+    },
+    #[error("Failed to extract 64-bit usize on this machine.")]
+    FailedToExtractSixtyFourBitUsize,
+    #[error("Unexpected number of bytes {bytes_length} for usize that expects either 4 or 8.")]
+    UnexpectedSizeOfUsize {
+        bytes_length: usize,
+    },
+    #[error("Unexpected byte value {byte_value} when trying to parse to boolean value of 0 or 1.")]
+    UnexpectedByteValueForBoolean {
+        byte_value: u8,
+    },
+}
+
+fn get_single_byte(bytes: &Vec<u8>, index: &mut usize) -> Result<u8, Box<dyn Error>> {
+    let bytes_length = bytes.len();
+    let index_deref = *index;
+    let next_index = index_deref + 1;
+    if bytes_length < next_index {
+        return Err(ByteConError::IndexOutOfRange {
+            index: next_index,
+            length: bytes_length,
+        }.into());
+    }
+    let byte = bytes[index_deref];
+    *index = next_index;
+    Ok(byte)
+}
+
+fn get_multiple_bytes<'a>(bytes: &'a Vec<u8>, index: &mut usize, size: usize) -> Result<&'a [u8], Box<dyn Error>> {
+    let bytes_length = bytes.len();
+    let index_deref = *index;
+    let next_index = index_deref + size;
+    if bytes_length < next_index {
+        return Err(ByteConError::IndexOutOfRange {
+            index: next_index,
+            length: bytes_length,
+        }.into());
+    }
+    let multiple_bytes = &bytes[index_deref..next_index];
+    *index = next_index;
+    Ok(multiple_bytes)
+}
+
+impl ByteCon for bool {
+    fn append_to_bytes(&self, bytes: &mut Vec<u8>) -> Result<(), Box<dyn Error>> {
+
+        // byte
+        bytes.push(if *self { 1 } else { 0 });
+        Ok(())
+    }
+    fn extract_from_bytes(bytes: &Vec<u8>, index: &mut usize) -> Result<Self, Box<dyn Error>> where Self: Sized {
+
+        // byte
+        let byte = get_single_byte(bytes, index)?;
+
+        let bool = match byte {
+            0 => false,
+            1 => true,
+            _ => {
+                return Err(ByteConError::UnexpectedByteValueForBoolean {
+                    byte_value: byte,
+                }.into());
+            },
+        };
+
+        Ok(bool)
+    }
+}
+
+impl ByteCon for f32 {
+    fn append_to_bytes(&self, bytes: &mut Vec<u8>) -> Result<(), Box<dyn Error>> {
+        
+        // f32
+        bytes.extend_from_slice(&self.to_le_bytes());
+        Ok(())
+    }
+    fn extract_from_bytes(bytes: &Vec<u8>, index: &mut usize) -> Result<Self, Box<dyn Error>> where Self: Sized {
+        
+        // f32
+        let float_bytes = get_multiple_bytes(bytes, index, 4)?;
+        let float = f32::from_le_bytes(float_bytes.try_into().expect("Failed to splice out bytes."));
+        Ok(float)
+    }
+}
+
+impl ByteCon for i8 {
+    fn append_to_bytes(&self, bytes: &mut Vec<u8>) -> Result<(), Box<dyn Error>> {
+        
+        // i8
+        bytes.push((*self) as u8);
+        Ok(())
+    }
+    fn extract_from_bytes(bytes: &Vec<u8>, index: &mut usize) -> Result<Self, Box<dyn Error>> where Self: Sized {
+        
+        let u8_byte = get_single_byte(bytes, index)?;
+        let i8_byte = u8_byte as i8;
+        Ok(i8_byte)
+    }
+}
+
+impl ByteCon for String {
+    fn append_to_bytes(&self, bytes: &mut Vec<u8>) -> Result<(), Box<dyn Error>> {
+        let string_bytes = self.as_bytes();
+        let string_bytes_length = string_bytes.len();
+
+        // usize
+        bytes.extend(&string_bytes_length.to_le_bytes());
+
+        // slice
+        bytes.extend_from_slice(string_bytes);
+        Ok(())
+    }
+    fn extract_from_bytes(bytes: &Vec<u8>, index: &mut usize) -> Result<Self, Box<dyn Error>> where Self: Sized {
+        
+        // usize
+        let string_bytes_length = usize::extract_from_bytes(bytes, index)?;
+        
+        // slice
+        let string_bytes = get_multiple_bytes(bytes, index, string_bytes_length)?;
+
+        Ok(Self::from_utf8(string_bytes.to_vec()).expect("Failed to convert bytes to a String."))
+    }
+}
+
+impl ByteCon for u8 {
+    fn append_to_bytes(&self, bytes: &mut Vec<u8>) -> Result<(), Box<dyn Error>> {
+        bytes.push(*self);
+        Ok(())
+    }
+    fn extract_from_bytes(bytes: &Vec<u8>, index: &mut usize) -> Result<Self, Box<dyn Error>> where Self: Sized {
+        let byte = get_single_byte(bytes, index)?;
+        Ok(byte)
+    }
+}
+
+impl ByteCon for usize {
+    fn append_to_bytes(&self, bytes: &mut Vec<u8>) -> Result<(), Box<dyn Error>> {
+
+        if cfg!(target_pointer_width = "64") {
+            // byte
+            bytes.push(8);
+
+            // 8 bytes
+            bytes.extend(&(*self as u64).to_le_bytes());
+        }
+        else {
+            // byte
+            bytes.push(4);
+
+            // 4 bytes
+            bytes.extend(&(*self as u32).to_le_bytes());
+        }
+        Ok(())
+    }
+    fn extract_from_bytes(bytes: &Vec<u8>, index: &mut usize) -> Result<Self, Box<dyn Error>> where Self: Sized {
+        
+        let usize_length = get_single_byte(bytes, index)? as usize;
+
+        if !cfg!(target_pointer_width = "64") && usize_length == 8 {
+            return Err(ByteConError::FailedToExtractSixtyFourBitUsize.into());
+        }
+
+        let usize_bytes = get_multiple_bytes(bytes, index, usize_length)?;
+
+        let usize_value = match usize_length {
+            8 => {
+                usize::try_from(u64::from_le_bytes(usize_bytes.try_into().expect("Failed to splice out bytes."))).expect("Failed to cast 8 bytes to usize.")
+            },
+            4 => {
+                usize::try_from(u32::from_le_bytes(usize_bytes.try_into().expect("Failed to splice out bytes."))).expect("Failed to cast 4 bytes to usize.")
+            },
+            _ => {
+                return Err(ByteConError::UnexpectedSizeOfUsize {
+                    bytes_length: usize_length,
+                }.into());
+            }
+        };
+
+        Ok(usize_value)
+    }
+}
+
+impl<T: ByteCon> ByteCon for Vec<T> {
+    fn append_to_bytes(&self, bytes: &mut Vec<u8>) -> Result<(), Box<dyn Error>> {
+
+        // usize
+        self.len().append_to_bytes(bytes)?;
+
+        for element in self {
+            // T
+            element.append_to_bytes(bytes)?;
+        }
+
+        Ok(())
+    }
+    fn extract_from_bytes(bytes: &Vec<u8>, index: &mut usize) -> Result<Self, Box<dyn Error>> where Self: Sized {
+        
+        // usize
+        let list_length = usize::extract_from_bytes(bytes, index)?;
+
+        let mut list = Vec::with_capacity(list_length);
+        for _ in 0..list_length {
+            // T
+            let list_element = T::extract_from_bytes(bytes, index)?;
+            list.push(list_element);
+        }
+
+        Ok(list)
+    }
+}
+
+// TODO uncomment once the nightly stability of overriding specialization is pushed into stable Rust
+//impl ByteCon for Vec<usize> {
+//    fn append_to_bytes(&self, bytes: &mut Vec<u8>) -> Result<(), Box<dyn Error>> {
+//
+//        let self_length = self.len();
+//        
+//        // usize of list
+//        self_length.append_to_bytes(bytes)?;
+//
+//        if self_length != 0 {
+//
+//            // usize of element
+//            if cfg!(target_pointer_width = "64") {
+//
+//                // byte
+//                bytes.push(8);
+//
+//                // 8 bytes
+//                for element in self {
+//                    bytes.extend(&(*element as u64).to_le_bytes());
+//                }
+//            }
+//            else {
+//
+//                // byte
+//                bytes.push(4);
+//
+//                // 4 bytes
+//                for element in self {
+//                    bytes.extend(&(*element as u32).to_le_bytes());
+//                }
+//            }
+//        }
+//        
+//        Ok(())
+//    }
+//    fn extract_from_bytes(bytes: &Vec<u8>, index: &mut usize) -> Result<Self, Box<dyn Error>> where Self: Sized {
+//        
+//        // usize of list
+//        let self_length = usize::extract_from_bytes(bytes, index)?;
+//
+//        if self_length == 0 {
+//            return Ok(Vec::new());
+//        }
+//
+//        // usize length of element
+//        let usize_length = get_single_byte(bytes, index)? as usize;
+//
+//        if !cfg!(target_pointer_width = "64") && usize_length == 8 {
+//            return Err(ByteConError::FailedToExtractSixtyFourBitUsize.into());
+//        }
+//
+//        let list = Vec::with_capacity(self_length);
+//        for _ in 0..self_length {
+//            let usize_bytes = get_multiple_bytes(bytes, index, usize_length)?;
+//
+//            let usize_value = match usize_length {
+//                8 => {
+//                    usize::try_from(u64::from_le_bytes(usize_bytes.try_into().expect("Failed to splice out bytes."))).expect("Failed to cast 8 bytes to usize.")
+//                },
+//                4 => {
+//                    usize::try_from(u32::from_le_bytes(usize_bytes.try_into().expect("Failed to splice out bytes."))).expect("Failed to cast 4 bytes to usize.")
+//                },
+//                _ => {
+//                    return Err(ByteConError::UnexpectedSizeOfUsize {
+//                        bytes_length: usize_length,
+//                    }.into());
+//                }
+//            };
+//        }
+//        Ok(list)
+//    }
+//}
