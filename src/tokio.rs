5c0ba223a478d02affb92bc36dd0f981da50bdf7
austin <galaky2005@gmail.com>
2024-11-10 20:53:06 -0500

    fixing TlsStream read and write;

--- src/tokio.rs
+++ src/tokio.rs
@@ -5,19 +5,64 @@ use crate::{ByteConverter, ByteStreamReaderAsync, ByteStreamWriterAsync};
 
 impl<TStream: AsyncWrite + AsyncRead + Unpin> ByteStreamReaderAsync for TlsStream<TStream> {
     async fn read_to_byte_converter<T: ByteConverter>(&mut self) -> Result<T, Box<dyn std::error::Error>> {
-        let mut bytes = Vec::new();
-        let mut chunk = [0u8; 64];
 
-        let mut initial_packet = [0u8; 8];
-        let read_result = self.read_exact(&mut initial_packet)
-            .await;
-        if let Err(error) = read_result {
-            let result: Result<T, Box<dyn Error>> = Err(Box::new(error));
-            return result;
+        let usize_length = {
+            // byte
+            let mut single_byte_chunk = [0u8; 1];
+            let read_result = self.read_exact(&mut single_byte_chunk)
+                .await;
+            if let Err(error) = read_result {
+                let result: Result<T, Box<dyn Error>> = Err(Box::new(error));
+                return result;
+            }
+
+            // length of usize
+            single_byte_chunk[0]
+        } as usize;
+
+        if !cfg!(target_pointer_width = "64") && usize_length == 8 {
+            return Err(TokioByteConError::FailedToExtractSixtyFourBitUsize.into());
         }
 
-        let expected_bytes_length: u64 = u64::from_le_bytes(initial_packet);
-        while (bytes.len() as u64) < expected_bytes_length {
+        let expected_bytes_length = match usize_length {
+            8 => {
+                // 8 bytes
+                let mut initial_packet = [0u8; 8];
+                let read_result = self.read_exact(&mut initial_packet)
+                    .await;
+                if let Err(error) = read_result {
+                    let result: Result<T, Box<dyn Error>> = Err(Box::new(error));
+                    return result;
+                }
+
+                // u64
+                let u64_instance = u64::from_le_bytes(initial_packet);
+                usize::try_from(u64_instance)?
+            },
+            4 => {
+                // 4 bytes
+                let mut initial_packet = [0u8; 4];
+                let read_result = self.read_exact(&mut initial_packet)
+                    .await;
+                if let Err(error) = read_result {
+                    let result: Result<T, Box<dyn Error>> = Err(Box::new(error));
+                    return result;
+                }
+
+                // u32
+                let u32_instance = u32::from_le_bytes(initial_packet);
+                usize::try_from(u32_instance)?
+            },
+            _ => {
+                return Err(TokioByteConError::UnexpectedSizeOfUsize {
+                    bytes_length: usize_length,
+                }.into());
+            }
+        };
+
+        let mut bytes = Vec::new();
+        let mut chunk = [0u8; 64];
+        while bytes.len() < expected_bytes_length {
             let read_bytes_length_result = self.read(&mut chunk)
                 .await;
             if let Err(error) = read_bytes_length_result {
@@ -39,9 +84,13 @@ impl<TStream: AsyncWrite + AsyncRead + Unpin> ByteStreamReaderAsync for TlsStrea
 
 impl<TStream: AsyncWrite + AsyncRead + Unpin> ByteStreamWriterAsync for TlsStream<TStream> {
     async fn write_from_byte_converter(&mut self, byte_converter: &impl crate::ByteConverter) -> Result<(), Box<dyn Error>> {
-        let mut stream_bytes = Vec::new();
-        byte_converter.append_to_bytes(&mut stream_bytes)?;
-        self.write(&stream_bytes)
+        let mut byte_converter_bytes = Vec::new();
+        byte_converter.append_to_bytes(&mut byte_converter_bytes)?;
+        let byte_converter_bytes_length = byte_converter_bytes.len();
+        let mut bytes = Vec::new();
+        byte_converter_bytes_length.append_to_bytes(&mut bytes)?;
+        byte_converter_bytes.append_to_bytes(&mut bytes)?;
+        self.write(&bytes)
             .await?;
         Ok(())
     }
@@ -71,4 +120,10 @@ impl ByteStreamWriterAsync for tokio::sync::mpsc::Sender<Vec<u8>> {
 enum TokioByteConError {
     #[error("Option variant None received from receiver.")]
     OptionVariantNoneReceivedFromReceiver,
+    #[error("Failed to extract 64-bit usize on this machine.")]
+    FailedToExtractSixtyFourBitUsize,
+    #[error("Unexpected number of bytes {bytes_length} for usize that expects either 4 or 8.")]
+    UnexpectedSizeOfUsize {
+        bytes_length: usize,
+    },
 }
